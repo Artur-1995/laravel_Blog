0dddc07b3caff68ceff6eb624161816eb4768641
Artur Avetisyan <89254423508@mail.ru>
2024-08-05 11:07:14 +0300

    Исправление верстки страницы личного кабинета

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -3,26 +3,27 @@
     'menu' => $menu,
 ])
 
+<div class="flex-1 container mx-auto bg-white overflow-hidden px-4 sm:px-6">
+    <a href="getToken/{{ $user['id'] }}"
+        class="inline-block bg-orange hover:bg-opacity-70 focus:outline-none text-white font-bold py-2 px-4 rounded"
+        role="button">
+        Получить Api Token
+    </a>
 
-<a href="getToken/{{ $user['id'] }}"
-    class="inline-block bg-orange hover:bg-opacity-70 focus:outline-none text-white font-bold py-2 px-4 rounded"
-    role="button">
-    Получить Api Token
-</a>
+    <?php
+    $hiddenText = $user['api_token'];
+    ?>
 
-<?php
-$hiddenText = $user['api_token'];
-?>
-
-@if ($user['api_token'])
-    <button class="inline-block bg-orange hover:bg-opacity-70 focus:outline-none text-white font-bold py-2 px-4 rounded"
-        role="button" onclick="copyToken('{{ $user['api_token'] }}')">Копировать токен</button>
-    <div class="py-3">
-        <p>Ваш токен:</p>
-        <p id="hiddenText">{{ $user['api_token'] }}</p>
-    </div>
-@endif
-
+    @if ($user['api_token'])
+        <button
+            class="inline-block bg-orange hover:bg-opacity-70 focus:outline-none text-white font-bold py-2 px-4 rounded"
+            role="button" onclick="copyToken('{{ $user['api_token'] }}')">Копировать токен</button>
+        <div class="py-3">
+            <p>Ваш токен:</p>
+            <p id="hiddenText">{{ $user['api_token'] }}</p>
+        </div>
+    @endif
+</div>
 <!-- Подключение jQuery -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
